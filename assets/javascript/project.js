b3594fc07f6eef3e9d18b46bd369c7fd0bb4f662
Michael Schumann <schumann82@gmail.com>
2018-06-16 14:25:13 -0700

    database set up

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCEoxS3DAffvPwmi9CA6xRtlLbXos6VsdE",
+    authDomain: "thewicketnailers.firebaseapp.com",
+    databaseURL: "https://thewicketnailers.firebaseio.com",
+    projectId: "thewicketnailers",
+    storageBucket: "",
+    messagingSenderId: "12069449119"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  database.ref().push({
+     // first part is child of database. after : is the value.
+  });
+
 
 }); // end of document.ready
 
