16335d5a02b3b6ffcd4c7c7c90a08408a0f9e321
Michael Schumann <schumann82@gmail.com>
2018-06-23 11:44:52 -0700

    ny times

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -14,10 +14,7 @@ $(document).ready(function() {
   var database = firebase.database();
 
   var searchTerm = "";
-  var slack = "";
-  var email = "";
-  var name = "";
-  var alert = "Not valid input!";
+ 
 
   $("#search-data").on("click", function(event) {
     // Don't refresh the page!
@@ -25,9 +22,7 @@ $(document).ready(function() {
     
 
     searchTerm = $("#search-input").val().trim();
-    name = $("#name-input").val().trim();
-    email = $("#email-input").val().trim();
-    slack = $("#slack-input").val().trim();
+    
 
     validate();
 
@@ -41,12 +36,12 @@ $(document).ready(function() {
 
     // Add database data to table data elements
     var newSearch = $("<td>").text(snapshot.val().searchTerm);
-    var newSlack = $("<td>").text(snapshot.val().slack);
+    
     
     
 
     // Add table data elements with database info to the table row
-    newRow.prepend(newSearch, newSlack);
+    newRow.prepend(newSearch);
 
     // Add the filled table row to the table
     $("#trending").prepend(newRow); // again not sure what the id will be but a placeholder.
@@ -62,32 +57,19 @@ function validate() {
 
   // Get the value of the input field with id="numb"
   searchTerm = $("#search-input").val().trim();
-  name = $("#name-input").val().trim();
-  email = $("#email-input").val().trim();
-  slack = $("#slack-input").val().trim();
+  
 
   if (((typeof searchTerm != "undefined") &&
      (typeof searchTerm.valueOf() == "string")) &&
-    (searchTerm.length > 0) && (typeof name != "undefined") &&
-    (typeof name.valueOf() == "string") &&
-   (name.length > 0) && (typeof email != "undefined") &&
-   (typeof email.valueOf() == "string") &&
-  (email.length > 0) && (typeof slack != "undefined") &&
-  (typeof slack.valueOf() == "string") &&
- (slack.length > 0)) {
-   if (validateEmail(email) === true) {
+    (searchTerm.length > 0)) {
+   
     pushData();
+    displayInfo();
+    displaytweets();
    }
-     else {
-      text = "Not valid email.";
-      $("#alert").append(text);
-      two = setTimeout(function twoSeconds(){
-        $("#alert").empty();
-      }, 2000);
-     }
-
-  } else {
-    text = "Fields not completed.";
+     
+   else {
+    text = "Search not valid.";
     $("#alert").append(text);
     two = setTimeout(function twoSeconds(){
       $("#alert").empty();
@@ -101,76 +83,101 @@ function validate() {
 function pushData() {
 
   searchTerm = $("#search-input").val().trim();
-  name = $("#name-input").val().trim();
-  email = $("#email-input").val().trim();
-  slack = $("#slack-input").val().trim();
+  
 
 
   database.ref().push({
         
     searchTerm: searchTerm,
-    slack: slack,
-    name: name,
-    email: email,
+    
      // can create additional fields as needed.
 
       
   });
 
   console.log(searchTerm);
-  console.log(slack);
-  console.log(name);
-  console.log(email);
+  
 
 }
 
 
 
-function validateEmail(email) {
-  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-  console.log(email);
-  return re.test(email);
 
-};
+function displayInfo() {
+    $("#results").empty();
+    var limit = 10;
 
-//Need to Limit to 10 and fix frame
-//Then pull simplified list to results div 
+    searchTerm = $("#search-input").val().trim();
+    console.log(searchTerm + "log");
+    var queryURL = "https://api.gdeltproject.org/api/v2/doc/doc?query=" + searchTerm + " sourcelang:english sourcecountry:US&mode=Artlist&TIMELINESMOOTH&TIMESPAN=1d=5&Sort=DateDesc&Maxrecords=" + limit + "&FORMAT=html";
 
-        
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      //console.log(response);
+      $("#results").append(response);
 
-        // <!-- <script type="text/javascript">
-        //     // $("#myFrame").attr('src', 'http://www.google.com/');
-        //     $("#myFrame").attr('src', "https://api.gdeltproject.org/api/v2/doc/doc?query=%22Trump%20President%22 sourcelang:english sourcecountry:US&mode=Artlist&TIMELINESMOOTH&TIMESPAN=1d=5&Sort=DateDesc&Maxrecords=10&FORMAT=html");
-        // </script> 
+    });
+  }
 
+  function displaytweets() {
+    $("#tweets").empty();
 
-        // <!-- JSON FORMAT -->
-        // <!-- https://api.gdeltproject.org/api/v2/doc/doc?query=%22Trump%20President%22%20sourcelang:english%20sourcecountry:US&mode=ArtList&TimelineVolInfo&TIMELINESMOOTH&TIMESPAN=1440=5&FORMAT=JSON -->
     
-        
+    searchTerm = $("#search-input").val().trim();
+    console.log(searchTerm + "log");
+    var key = "&api-key=11a88ca9be994975831753129dc04cf5";
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + key;
+    
+
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+       console.log(response);
+       var results = response.response.docs;
 
 
-// window.twttr = (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0],
-//       t = window.twttr || {};
-//     if (d.getElementById(id)) return t;
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://platform.twitter.com/widgets.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-  
-//     t._e = [];
-//     t.ready = function(f) {
-//       t._e.push(f);
-//     };
-// var twitterdm = "email-input"
-//     <a href="https://twitter.com/messages/compose?recipient_id=3805104374&text=Hello%20world"
-//   class="twitter-dm-button" data-screen-name="@davekubo">
-// Message @davekubo</a>
- 
+       for (var i = 0; i < results.length; i++) {
+          var topicDiv = $("<div>");
+          
+          var p = $("<p>").text(results[i].web_url);
+          
+          topicDiv.append(p);
+          $("#tweets").prepend(topicDiv);
+       }
 
-  //   return t;
-  // }(document, "script", "twitter-wjs"));
+
+      
+    })
+
+
+
+
+
+
+    // var queryURL = "https://api.twitter.com/1.1/search/tweets.json?q=" + searchTerm;
+
+    // console.log(queryURL);
+    // $.ajax({
+    //   url: queryURL,
+      
+       
+      
+    //   method: "GET"
+    // }).then(function (response) {
+    //   console.log(response);
+      
+
+    // });
+
+}
+
+
+
 
 
 
